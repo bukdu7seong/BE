66e042a483d3615727c33d075cdeff34c3f95fcb
weejihye <kikiwjh@naver.com>
2024-01-12 15:38:27 +0900

    init: postgres

--- ts/settings.py
+++ ts/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'ts.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ts',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '15432',
     }
 }
 
