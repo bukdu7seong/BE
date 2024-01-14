98defa20550b3de4714c78bf0aea86743eacc586
ekwak <cfcf26@naver.com>
2024-01-14 22:35:49 +0900

    Update user registration form and add image upload functionality

--- test_frontend/index.js
+++ test_frontend/index.js
@@ -1,36 +1,3 @@
-function addNewUser() {
-  var email = $('#newUserEmail').val();
-  var imageUrl = $('#newUserImageURL').val();
-  var nickname = $('#newUserNickname').val();
-  var accessToken = 'token_' + Math.random().toString(36).substr(2, 9);
-  var providerId = 'google_' + Math.random().toString(36).substr(2, 9);
-
-  $.ajax({
-    url: 'http://localhost:8000/add_new_user',
-    type: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify({
-      access_token: accessToken,
-      email: email,
-      provider: 'google',
-      provider_id: providerId,
-      image: imageUrl,
-      nickname: nickname
-    }),
-    success: function(response) {
-      $('#response')
-          .html('User added successfully: ' + JSON.stringify(response));
-    },
-    error: function(response) {
-      $('#response').html('Error adding user: ' + response.responseText);
-    }
-  });
-}
-
-$(document).ready(function() {
-  $('#addUserButton').click(addNewUser);
-});
-
 function showApiTestSection() {
   $('#apiTestSection').show();
 }
@@ -41,29 +8,31 @@ function getUserProfile() {
     alert('No token found. Please login first.');
     return;
   }
-
   $.ajax({
     url: 'http://localhost:8000/profile',
     type: 'GET',
     headers: {'Authorization': 'Bearer ' + token},
     success: function(response) {
-      $('#response').html(JSON.stringify(response));
+      var img = $('<img>', {src: response.image});
+      console.log(response);
+      $('#response').html(img);
     },
     error: function(error) {
+      console.log(error);
       $('#response').html('Error: ' + error.responseText);
     }
   });
 }
 
-$('#loginButton').click(function() {
+function loginUser() {
   var userId = $('#userId').val();
   var testToken = 'mockToken_' + userId;
 
   $.ajax({
-    url: 'http://localhost:8000/verify_token',
-    type: 'POST',
+    url: 'http://localhost:8000/login',
+    type: 'GET',
     contentType: 'application/json',
-    data: JSON.stringify({token: testToken}),
+    headers: {'Authorization': 'Bearer ' + testToken},
     success: function(response) {
       sessionStorage.setItem('mockToken', testToken);
       showApiTestSection();
@@ -73,109 +42,48 @@ $('#loginButton').click(function() {
       alert('Login failed: ' + response.responseText);
     }
   });
-});
-
-function searchUser(nickname) {
-  var token = sessionStorage.getItem('mockToken');
-  if (!token) {
-    $('#response').html('Error: No token found. Please login first.');
-    return;
-  }
-
-  $.ajax({
-    url: 'http://localhost:8000/get_user_by_nickname?nickname=' +
-        encodeURIComponent(nickname),
-    type: 'GET',
-    headers: {'Authorization': 'Bearer ' + token},
-    success: function(response) {
-      $('#response').html(JSON.stringify(response));
-      friendUserId = response.user_id;  // Assuming response contains user_id
-      $('#addFriendButton').show();
-    },
-    error: function(response) {
-      $('#response').html('Error: ' + response.responseText);
-    }
-  });
 }
 
-function sendFriendRequest() {
-  var token = sessionStorage.getItem('mockToken');
-  if (!token) {
-    $('#response').html('Error: No token found. Please login first.');
-    return;
-  }
+function addNewUser() {
+  var email = $('#newUserEmail').val();
+  var nickname = $('#newUserNickname').val();
+  var accessToken = 'token_' + Math.random().toString(36).substr(2, 9);
+  var providerId = 'google_' + Math.random().toString(36).substr(2, 9);
 
-  $.ajax({
-    url: 'http://localhost:8000/add_friend?nickname=' +
-        encodeURIComponent(friendUserId),
-    type: 'POST',
-    headers: {'Authorization': 'Bearer ' + token},
-    success: function(response) {
-      $('#response').html('Friend request sent: ' + JSON.stringify(response));
-    },
-    error: function(error) {
-      $('#response').html('Error: ' + JSON.stringify(error.responseJSON));
-    }
-  });
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var base64Image = e.target.result;
+    var img = $('<img>', {src: base64Image});
+    $('#response').html(img);
+
+    $.ajax({
+      url: 'http://localhost:8000/add_new_user',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({
+        email: email,
+        image: base64Image,
+        nickname: nickname,
+        access_token: accessToken,
+        provider_id: providerId
+      }),
+      success: function(response) {
+        $('#response')
+            .html('User added successfully: ' + JSON.stringify(response));
+      },
+      error: function(response) {
+        $('#response').html('Error adding user: ' + response.responseText);
+      }
+    });
+  };
+  reader.readAsDataURL($('#newUserImage')[0].files[0]);
 }
-
-function friendList() {
-  var token = sessionStorage.getItem('mockToken');
-  if (!token) {
-    $('#response').html('Error: No token found. Please login first.');
-    return;
-  }
-
-  $.ajax({
-    url: 'http://localhost:8000/get_friend_pending_list',
-    type: 'GET',
-    headers: {'Authorization': 'Bearer ' + token},
-    success: function(response) {
-      $('#response').html(JSON.stringify(response));
-    },
-    error: function(response) {
-      $('#response').html('Error: ' + response.responseText);
-    }
-  });
-}
-
 
 
 $(document).ready(function() {
-  $('#getUserProfile').click(function() {
-    getUserProfile();
-  });
-
-  $('#UpdateImage').click(function() {
-    var newImageUrl = $('#newImageUrl').val();
-    var token = sessionStorage.getItem('mockToken');
-
-    if (!token) {
-      $('#response').html('Error: No token found. Please login first.');
-      return;
-    }
-
-    $.ajax({
-      url: 'http://localhost:8000/update_image',
-      type: 'PUT',
-      contentType: 'application/json',
-      headers: {'Authorization': 'Bearer ' + token},
-      data: JSON.stringify({image: newImageUrl}),
-      success: function(response) {
-        $('#response').html(JSON.stringify(response));
-      },
-      error: function(response) {
-        $('#response').html('Error: ' + response.responseText);
-      }
-    });
-  });
-
-  $('#searchButton').click(function() {
-    var nickname = $('#search').val();
-    searchUser(nickname);
-  });
-
   $('#addUserButton').click(addNewUser);
-  $('#addFriendButton').click(sendFriendRequest);
-  $('#getFriendListButton').click(friendList);
 });
+
+// events
+
+$('#loginButton').click(loginUser);
