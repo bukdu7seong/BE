caf8d83045115a8afe44c95f03a883dcd662b5b7
ekwak <cfcf26@naver.com>
2024-02-21 22:57:05 +0900

    Refactor URL path for friend module endpoint Restructure the URL path for the friend module endpoint within the API to maintain a clear and organized URL structure. This change ensures better separation of concerns and supports future scalability. No related issues.

--- ts/urls.py
+++ ts/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # url reverse
-    path('friend/', include('friend.urls')),
+    path('api/friend/', include('friend.urls')),
     path('api/games/', include('game.urls')),
     path('account/', include('account.urls')),
 ]
