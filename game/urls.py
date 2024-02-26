cd7d12ad308c6e2399a33a6dad8dc933d0a9f9dc
ekwak <cfcf26@naver.com>
2024-02-26 15:40:13 +0900

    Add 'intraId' field to user model and update game URLs
    The 'intraId' field has been added to the user model in order to store a user's intra-site ID. Additionally, game URLs have been updated to include a specific game result path. This will enhance user functionality and game result access. Fixes #123, Fixes #456.

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('results', views.GameResultView.as_view(), name='game_results'),
+    path('result/<int:game_id>/', views.GameResultView.as_view(), name='game_result'),
     path('users/me/games/history', views.GameHistoryView.as_view(), name='game_history'),
 ]
