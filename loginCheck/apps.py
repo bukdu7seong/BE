a24edf0032bc909260e399e9a124bcb168c61fdd
weejihye <kikiwjh@naver.com>
2024-02-29 14:16:31 +0900

    fix: remove useless import

--- loginCheck/apps.py
+++ loginCheck/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-from django.dispatch import receiver
-from django.contrib.auth.signals import user_logged_in, user_logged_out
 
 
 class LogincheckConfig(AppConfig):
